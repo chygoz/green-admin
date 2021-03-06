fdea5d289d010b84308de5cb52f5c00d7bb2edb4
sainik18 <sainik18@gmail.com>
2021-03-06 21:29:39 +0530

    push

--- src/app/merchants/merchants.component.ts
+++ src/app/merchants/merchants.component.ts
@@ -5,16 +5,19 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 import { Router } from '@angular/router';
 import { NewMerchantComponent } from '../new-merchant/new-merchant.component';
 import { NewOutletComponent } from '../new-outlet/new-outlet.component';
+import { CookieService } from '../services/cookie.service';
 @Component({
   selector: 'app-merchants',
   templateUrl: './merchants.component.html',
   styleUrls: ['./merchants.component.scss']
 })
 export class MerchantsComponent implements OnInit {
-
-  constructor(location: Location,
-    public dialog: MatDialog,
-    private router: Router) { }
+  userData;
+  constructor(location: Location, public dialog: MatDialog, private router: Router, private cookieService: CookieService) {
+    this.userData = this.cookieService.getCookie('currentUser');
+    this.userData = JSON.parse(this.userData);
+    console.log(this.userData);
+   }
 
   ngOnInit(): void {
   }
