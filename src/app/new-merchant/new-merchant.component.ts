fc4995c7ede08cf5b51038222472a96f54f9aa1c
sainik18 <sainik18@gmail.com>
2021-03-07 13:33:02 +0530

    merchant validation added

--- src/app/new-merchant/new-merchant.component.ts
+++ src/app/new-merchant/new-merchant.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { apiService } from '../api.service';
+import { CookieService } from '../services/cookie.service';
+
+export function ConfirmedValidator(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+    if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+      return;
+    }
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ confirmedValidator: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  }
+}
 
 @Component({
   selector: 'app-new-merchant',
@@ -6,10 +24,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-merchant.component.scss']
 })
 export class NewMerchantComponent implements OnInit {
+  merchantForm: FormGroup;
+  formSubmit: boolean = false;
+  public emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  constructor() { }
+  constructor(public fb: FormBuilder, private service: apiService, private cookieService: CookieService) { }
 
   ngOnInit(): void {
+    //storeName, email, phoneNumber, userName, password
+    this.merchantForm = this.fb.group({
+      storeName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(this.emailregex)]],
+      phoneNumber: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    }, {
+      validator: ConfirmedValidator('password', 'confirmPassword')
+    })
+  }
+
+  get f() {
+    return this.merchantForm.controls;
+  }
+
+  onSubmit(){
+    this.formSubmit = true;
+    console.log(this.merchantForm.valid);
+    // if(!this.formSubmit.valid){
+    //   return false;
+    // }
+    console.log(this.merchantForm.value);
   }
 
 
