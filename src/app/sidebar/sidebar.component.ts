09762d0e1116a75645b8f9780fc32053ee518388
sainik18 <sainik18@gmail.com>
2021-03-07 15:55:53 +0530

    side bar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from '../services/cookie.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
+  userData;
+  constructor(private cookieService: CookieService) {
+    this.userData = this.cookieService.getCookie('currentUser');
+    this.userData = JSON.parse(this.userData);
+   }
 
   ngOnInit(): void {
   }
