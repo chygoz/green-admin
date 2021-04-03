f87c75f14f19488806e6663221e6d285233eb6ee
varavk <varavk@gmail.com>
2021-04-04 02:52:51 +0530

    bug fix

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,9 +2,5 @@
 export const config = {
     api_url: 'http://localhost:3000/api/'
     //api_url: 'https://green-njs.herokuapp.com/'
-<<<<<<< Updated upstream
-    api_url: 'https://greendiscountcard.com/api/'
-=======
     //api_url: 'http://greendiscountcard.com/api/'
->>>>>>> Stashed changes
 }
