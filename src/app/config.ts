2a7dcbe88d82a2d584f57a25023461bd6d41aa55
sainik18 <sainik18@gmail.com>
2021-04-04 00:22:14 +0530

    dist

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,5 @@
 export const config = {
     //api_url: 'http://localhost:3000/api/'
     //api_url: 'https://green-njs.herokuapp.com/'
-    api_url: 'http://greendiscountcard.com/api/'
+    api_url: 'https://greendiscountcard.com/api/'
 }
